c2c8954c1bd2222f58c68ccb6c4e1c7c1aa87d69
jein-sh <jein.sh85@gmail.com>
2023-04-29 16:07:07 +0200

    добавляет анимацию появления шапки и подвала

--- source/js/script.js
+++ source/js/script.js
@@ -8,8 +8,10 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import loadPage from './modules/load-page.js';
 
 // init modules
+loadPage();
 mobileHeight();
 slider();
 menu();
