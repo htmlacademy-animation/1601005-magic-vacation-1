4b5a17820042d862074a241d8e598aea2bee64d1
jein-sh <jein.sh85@gmail.com>
2023-05-04 16:50:08 +0200

    анимация акцентной типографики

---
+++ source/js/modules/screen-animation.js
@@ -0,0 +1,32 @@
+import { initTypographyAnimation } from "../animate/typography-animate";
+
+
+const introTitleAnimation = () => {
+  const title = document.querySelector('.intro__title');
+  initTypographyAnimation(title, 500, 'transform');
+}
+
+const dateAnimation = () => {
+  const date = document.querySelector('.intro__date');
+  initTypographyAnimation(date, 500, 'transform', false);
+}
+
+const titleAnimation = () => {
+  const titles = document.querySelectorAll('h2');
+
+  if(titles.length === 0) {
+    return;
+  }
+
+  titles.forEach((title) => {
+    initTypographyAnimation(title, 500, 'transform');
+  });
+}
+
+const initScreenAnimation = () => {
+  introTitleAnimation();
+  dateAnimation();
+  titleAnimation();
+}
+
+export default initScreenAnimation;
