68d8efe0012b296579c01112e4e279394509d6b9
jein-sh <jein.sh85@gmail.com>
2023-06-22 22:13:19 +0200

    анимация чисел на экране призы

--- source/js/modules/screen-prizes.js
+++ source/js/modules/screen-prizes.js
@@ -1,3 +1,72 @@
+const countCaseAnim = (countCase, parent) => {
+  const countEl = parent.querySelector('b');
+  let fps = 12;
+
+  let fpsInterval = 1000 / fps;
+  let now,
+    then = Date.now(),
+    elapsed;
+  let count = 1;
+  countEl.innerHTML = String(count);
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval && count < countCase) {
+      then = now - (elapsed % fpsInterval);
+      count += 1;
+      countEl.innerHTML = String(count);
+    }
+  };
+
+  requestAnimationFrame(tick);
+}
+
+const getRandomArr = (length, min, max) => {
+  const arr = [min];
+  for( let i=0 ; i < length - 2; i++) {
+    arr.push(Math.floor(Math.random() * (max - min) + min));
+  }
+  arr.sort((a, b) => a - b);
+  arr.push(max);
+
+  return arr;
+}
+
+const countCodeAnim = (min, max, parent) => {
+  const countEl = parent.querySelector('b');
+  let fps = 12;
+
+  let fpsInterval = 1000 / fps;
+  let now,
+    start = Date.now(),
+    then = start,
+    elapsed;
+  let count = 1;
+
+  countEl.innerHTML = String(min);
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval && count < fps - 1 ) {
+      then = now - (elapsed % fpsInterval);
+      count += 1;
+
+      const randomNumbers = getRandomArr(fps, min, max);
+      countEl.innerHTML = String(randomNumbers[count]);
+    }
+  };
+
+  requestAnimationFrame(tick);
+}
+
 const screenPrizes = () => {
   const prize1 = document.querySelector('.prizes__item--journeys');
   const prize2 = document.querySelector('.prizes__item--cases');
@@ -13,18 +82,27 @@ const screenPrizes = () => {
 
         setTimeout(() => {
           prize1.classList.remove('is-anim');
-        }, 8000)
+        }, 7000)
       }
 
       if (prize2 && prize2.childNodes.length === 0) {
         setTimeout(() => {
-          prize2.innerHTML = '<div class="prizes__icon"><img src="img/anim-case.svg" alt=""></div><p class="prizes__desc"><b>7</b><span>надежных чемоданов</span></p>'
-        }, 7500)
+          prize2.innerHTML = '<div class="prizes__icon"><img src="img/anim-case.svg" alt=""></div><p class="prizes__desc"><b>1</b><span>надежных чемоданов</span></p>'
+
+          setTimeout(() => {
+            countCaseAnim(7, prize2);
+          }, 1500);
+        }, 6500)
       }
 
       if (prize3 && prize3.childNodes.length === 0) {
         setTimeout(() => {
-          prize3.innerHTML = '<div class="prizes__icon"><img src="img/anim-code.svg" alt=""></div><p class="prizes__desc"><b>900</b><span>промокодов на&nbsp;скидку 15%</span></p>'
+          prize3.innerHTML = '<div class="prizes__icon"><img src="img/anim-code.svg" alt=""></div><p class="prizes__desc"><b>11</b><span>промокодов на&nbsp;скидку 15%</span></p>'
+
+          setTimeout(() => {
+            countCodeAnim(11, 900, prize3);
+          }, 1500);
+
         }, 10000);
       }
     }
